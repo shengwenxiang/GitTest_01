33be8a7f2aa1a8e0c0222d63d086d401d0f57e4c
Shaken <2281883042@qq.com>
2022-06-27 10:09:29 +0800

    First Commit

---
+++ src/main/java/com/swx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.swx.git;
+
+/**
+ * Description: GitTest_01
+ * Created by Shaken on 2022/6/27 9:51
+ * Email:2281883042@qq.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
